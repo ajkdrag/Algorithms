57295232df87d0702eb711ef7dacc865ba88e6b2
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2018-08-27 11:51:18 +0530

    Create Unlucky_13.java

---
+++ Coding Problems/Unlucky_13.java
@@ -0,0 +1,20 @@
+/*
+* Problem at : https://www.hackerearth.com/challenge/college/institute-coding-test-2-1/algorithm/80c3c4effdf94e5189a0d213c6127d05/
+* Basically we have to find the total number of such strings s that are made of exactly N characters, without the number 13
+* The strings may contain any integer from 0-9, repeated any number of times.
+* For example : if N = 2, i.e number of 2 digit numbers that do not contain 13 = 99 (since we only get one occurence 13)
+* if N = 1, we get result as 10, since between 0 and 9 (inclusive), we don't get any occurence of 13
+* The output file should contain answer to each query in a new line modulo 1000000009.
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
