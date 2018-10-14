1b11e5128db3d45713e49bc5c21592936059d72a
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2018-10-14 17:56:54 +0530

    Update Problem34.java

--- Project_Euler_Solutions/Problem34.java
+++ Project_Euler_Solutions/Problem34.java
@@ -68,3 +68,98 @@ class Solution34 {
 }
 
 
+/*
+
+  Non-recursive solution (same as above, but generating of combinations is done iteratively)
+
+*/
+
+public class Problem34 {
+    public static void main(String args[]) {
+        System.out.println(Solution34.solve());
+    }
+}
+
+class Solution34 {
+    static int[] fact = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+    static int solve(){
+        int[] chosen =  new int[7];
+        int limit = 11440;
+        int res = 0;
+        while(limit-- > 0){
+            int sum = 0;
+            int num = 0;
+            boolean startCounting = false;
+            for (int i = 0; i < 7; ++i){
+                int val = chosen[i];
+                num = num*10 + val;
+                if(val > 0) startCounting = true;
+                if(startCounting) sum += fact[val];
+            }
+            increment(chosen);
+            if(sum < num || sum < 10) continue;
+            if(calcFactSum(sum) == sum){
+                res += sum;
+            } 
+            if(chosen[0] == 1 && chosen[1] == 5) break;
+        }
+        return res;
+    }
+    
+    static void disp(int[] chosen){
+        for(int i = 0; i < chosen.length; ++i) System.out.print(chosen[i] + " ");
+    }
+    
+    static void increment(int[] chosen){
+        int carry = 1;
+        int x = -1;
+        int s = -1;
+        for(int i = chosen.length - 1; i >= 0; --i){
+            int val = chosen[i];
+            val += carry;
+            carry = 0;
+            if(val == 10){
+                val = 0;
+                carry = 1;
+            }
+            chosen[i] = val;
+            if(carry == 0) {
+                s = chosen.length - 1 - i;
+                x = val;
+                break;
+            }
+        }
+        
+        int extraJumps = calcExtraJumps(x,s);
+        if(extraJumps!=0) increment(chosen, extraJumps);
+    }
+    
+    static int calcExtraJumps(int x, int s){
+        if(s == 0) return 0;
+        if(s == 1) return x;
+        
+        int temp = ((int)Math.pow(10,s)) - 1;
+        return (temp/9)*x;
+    }
+    
+    static void increment(int[] chosen, int carry){
+        for(int i = chosen.length - 1; i >= 0; --i){
+            int val = chosen[i];
+            val += carry;
+            chosen[i] = val%10;
+            carry = val/10;
+            if(carry == 0) break;
+        }
+    }
+    
+    static int calcFactSum(int num){
+        int sum = 0;
+        while(num > 0){
+            sum += fact[num%10];
+            num/=10;
+        }
+        return sum;
+    }
+}
+
+
