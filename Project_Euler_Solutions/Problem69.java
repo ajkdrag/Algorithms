cd14464e54318277f2110b739193b2315f6d0af4
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2018-11-08 01:15:45 +0530

    Create Problem69.java

---
+++ Project_Euler_Solutions/Problem69.java
@@ -0,0 +1,42 @@
+/*
+
+  Problem at : https://projecteuler.net/problem=69
+
+*/
+
+public class Problem69 {
+    public static void main(String args[]) {
+        System.out.println(Solution69.solve(1000004));
+    }
+}
+
+class Solution69 {
+    static int[] phiSieve;
+    static int solve(int limit){
+        phiSieve = new int[limit];
+        phiSieve[1] = 1;
+        for(int i = 2; i <= limit>>1; ++i){
+            if(phiSieve[i] == 0){
+                for(int j = i<<1; j < limit; j+=i){
+                    int val = phiSieve[j];
+                    if(val == 0) val = j;
+                    val -= val/i;
+                    phiSieve[j] = val;
+                }
+            }
+        }
+        
+        long n = 6;
+        long phi = 2;
+        for(long k = 10; k < limit; k += 4){
+            long phi_k = phiSieve[(int)k];
+            if(k*phi >= n*phi_k){
+                n = k;
+                phi = phi_k;
+            }
+        }
+        return (int)n;
+    }
+}
+
+
