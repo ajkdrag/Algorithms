d53f981cfd7234a8c668a6959e659b0e5eb0d0fb
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2018-11-09 11:44:03 +0530

    Create Problem71.java

---
+++ Project_Euler_Solutions/Problem71.java
@@ -0,0 +1,34 @@
+/*
+
+  Problem at : https://projecteuler.net/problem=71
+
+*/
+
+public class Problem71 {
+    public static void main(String args[]) {
+        System.out.println(Solution71.solve(3,7,1000000));        
+    }
+}
+
+class Solution71 {
+    static long solve(long numer, long deno, long limit){
+        double frac = numer*1.0/deno;
+        long max_n = 1;
+        long max_d = limit;
+        double temp = 0.0;
+        long x = 0L;
+        
+        while(limit-- > 2){
+            temp = (frac*limit);
+            x = (long)temp;
+            if(x == (long)temp) --x;
+            if(x*max_d > max_n*limit){
+                max_n = x;
+                max_d = limit;
+            }
+        }
+        return max_n;
+    }
+}
+
+
