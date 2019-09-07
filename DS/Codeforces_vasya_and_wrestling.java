0dba2be1bd004402d2d913c54bf2b8cf649e7410
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-09-07 18:45:20 +0530

    Create Codeforces_vasya_and_wrestling.java

---
+++ DS/Codeforces_vasya_and_wrestling.java
@@ -0,0 +1,192 @@
+/*
+  Problem at : https://codeforces.com/contest/493/problem/B
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scan sc = new Scan();
+        int n = sc.scanInt();
+        Sol sol = new Sol(n);
+        while(n-- > 0){
+            int val = sc.scanInt();
+            if(val > 0)
+                sol.putFirst(val);
+            else 
+                sol.putSecond(-val);
+        }
+        sol.solve();
+    }
+}
+
+class Sol {
+    int[] first, second;
+    long sumFirst, sumSecond;
+    boolean found;
+    int i, j, k, winner, last;
+    
+    Sol(int n ){
+        first = new int[n+1];
+        second = new int[n+1];
+    }
+    
+    void putFirst(int a){
+        first[i++] = a;
+        sumFirst += a;
+        
+        if(second[k] > first[k]){
+            winner = 2;
+        }
+        else if(second[k] < first[k]){
+            winner = 1;
+        }
+        else {
+            ++k;
+        }
+        
+        last = 1;
+    }
+    
+    void putSecond(int a){
+        second[j++] = a;
+        sumSecond += a;
+        
+        if(second[k] > first[k]){
+            winner = 2;
+        }
+        else if(second[k] < first[k]){
+            winner = 1;
+        }
+        else {
+            ++k;
+        }
+        
+        last = 2;
+    }
+    
+    void solve(){
+        if(sumFirst > sumSecond){
+            System.out.println("first");
+            return;
+        }
+        if(sumFirst < sumSecond){
+            System.out.println("second");
+            return;
+        }
+        if(k < i){
+            System.out.println(winner == 1 ? "first" : "second");
+            return;
+        }
+        System.out.println(last == 1 ? "first" : "second");
+    }
+}
+
+class Scan
+{
+    private byte[] buf=new byte[1024];
+    private int index;
+    private InputStream in;
+    private int total;
+    public Scan()
+    {
+        in=System.in;
+    }
+    public int scan()throws IOException
+    {
+        if(total<0)
+        throw new InputMismatchException();
+        if(index>=total)
+        {
+            index=0;
+            total=in.read(buf);
+            if(total<=0)
+            return -1;
+        }
+        return buf[index++];
+    }
+    public int scanInt()throws IOException
+    {
+        int integer=0;
+        int n=scan();
+        while(isWhiteSpace(n))
+        n=scan();
+        int neg=1;
+        if(n=='-')
+        {
+            neg=-1;
+            n=scan();
+        }
+        while(!isWhiteSpace(n))
+        {
+            if(n>='0'&&n<='9')
+            {
+                integer*=10;
+                integer+=n-'0';
+                n=scan();
+            }
+            else throw new InputMismatchException();
+        }
+        return neg*integer;
+    }
+    public double scanDouble()throws IOException
+    {
+        double doub=0;
+        int n=scan();
+        while(isWhiteSpace(n))
+        n=scan();
+        int neg=1;
+        if(n=='-')
+        {
+            neg=-1;
+            n=scan();
+        }
+        while(!isWhiteSpace(n)&&n!='.')
+        {
+            if(n>='0'&&n<='9')
+            {
+                doub*=10;
+                doub+=n-'0';
+                n=scan();
+            }
+            else throw new InputMismatchException();
+        }
+        if(n=='.')
+        {
+            n=scan();
+            double temp=1;
+            while(!isWhiteSpace(n))
+            {
+                if(n>='0'&&n<='9')
+                {
+                    temp/=10;
+                    doub+=(n-'0')*temp;
+                    n=scan();
+                }
+                else throw new InputMismatchException();
+            }
+        }
+        return doub*neg;
+    }
+    public String scanString()throws IOException
+    {
+        StringBuilder sb=new StringBuilder();
+        int n=scan();
+        while(isWhiteSpace(n))
+        n=scan();
+        while(!isWhiteSpace(n))
+        {
+            sb.append((char)n);
+            n=scan();
+        }
+        return sb.toString();
+    }
+    private boolean isWhiteSpace(int n)
+    {
+        if(n==' '||n=='\n'||n=='\r'||n=='\t'||n==-1)
+        return true;
+        return false;
+    }
+}
+
