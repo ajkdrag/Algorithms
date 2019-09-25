5723261265143344b2b641ccafb535e56b77723c
Altamash Khan <40544701+aizenkhan@users.noreply.github.com>
2019-09-25 13:05:00 +0530

    Create Codeforces_T_primes.java

---
+++ BinarySearch/Codeforces_T_primes.java
@@ -0,0 +1,145 @@
+/*
+  Problem at : https://codeforces.com/contest/230/problem/B
+*/
+
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scan sc = new Scan();
+        long n = sc.scanLong();
+        while(n-- > 0){
+            long val = sc.scanLong();
+            if(val < 4){
+                System.out.println("NO");
+                continue;
+            }
+            long sqrt = (int)Math.sqrt(val);
+            if(sqrt*sqrt == val && isPrime(sqrt))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+    
+    static boolean isPrime(long n) {
+        if(n < 2) return false;
+        if(n == 2 || n == 3) return true;
+        if(n%2 == 0 || n%3 == 0) return false;
+        long sqrtN = (long)Math.sqrt(n)+1;
+        for(long i = 6L; i <= sqrtN; i += 6) {
+            if(n%(i-1) == 0 || n%(i+1) == 0) return false;
+        }
+        return true;
+    }
+}
+ 
+ 
+class Scan
+{
+    private byte[] buf=new byte[1024];
+    private int index;
+    private InputStream in;
+    private int total;
+    public Scan()
+    {
+        in=System.in;
+    }
+    public int scan()throws IOException
+    {
+        if(total<0)
+        throw new InputMismatchException();
+        if(index>=total)
+        {
+            index=0;
+            total=in.read(buf);
+            if(total<=0)
+            return -1;
+        }
+        return buf[index++];
+    }
+    public long scanLong()throws IOException
+    {
+        long integer=0;
+        int n=scan();
+        while(isWhiteSpace(n))
+        n=scan();
+        int neg=1;
+        if(n=='-')
+        {
+            neg=-1;
+            n=scan();
+        }
+        while(!isWhiteSpace(n))
+        {
+            if(n>='0'&&n<='9')
+            {
+                integer*=10;
+                integer+=n-'0';
+                n=scan();
+            }
+            else throw new InputMismatchException();
+        }
+        return neg*integer;
+    }
+    public double scanDouble()throws IOException
+    {
+        double doub=0;
+        int n=scan();
+        while(isWhiteSpace(n))
+        n=scan();
+        int neg=1;
+        if(n=='-')
+        {
+            neg=-1;
+            n=scan();
+        }
+        while(!isWhiteSpace(n)&&n!='.')
+        {
+            if(n>='0'&&n<='9')
+            {
+                doub*=10;
+                doub+=n-'0';
+                n=scan();
+            }
+            else throw new InputMismatchException();
+        }
+        if(n=='.')
+        {
+            n=scan();
+            double temp=1;
+            while(!isWhiteSpace(n))
+            {
+                if(n>='0'&&n<='9')
+                {
+                    temp/=10;
+                    doub+=(n-'0')*temp;
+                    n=scan();
+                }
+                else throw new InputMismatchException();
+            }
+        }
+        return doub*neg;
+    }
+    public String scanString()throws IOException
+    {
+        StringBuilder sb=new StringBuilder();
+        int n=scan();
+        while(isWhiteSpace(n))
+        n=scan();
+        while(!isWhiteSpace(n))
+        {
+            sb.append((char)n);
+            n=scan();
+        }
+        return sb.toString();
+    }
+    private boolean isWhiteSpace(int n)
+    {
+        if(n==' '||n=='\n'||n=='\r'||n=='\t'||n==-1)
+        return true;
+        return false;
+    }
+}
+
